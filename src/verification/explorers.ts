2679c0a0c15aebbe5d96aa195b95c25c259a48dc
0xmikko <26343374+0xmikko@users.noreply.github.com>
2024-04-03 11:44:26 +0200

    fix: optimistic etherscan fixed

--- src/verification/explorers.ts
+++ src/verification/explorers.ts
@@ -3,6 +3,6 @@ import { NetworkType } from "../core/chains";
 export const explorerUrl: Record<NetworkType, string> = {
   Mainnet: "https://api.etherscan.io/api",
   Arbitrum: "https://api.arbiscan.io/api",
-  Optimism: "https://api.optimistic.etherscan.io/api",
+  Optimism: "https://api-optimistic.etherscan.io/api",
   Base: "https://api.basescan.org/api",
 };
