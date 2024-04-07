8f0c448fd7a8af564fd0782b922c49e299e4023d
0xmikko <26343374+0xmikko@users.noreply.github.com>
2024-04-07 22:58:46 +0200

    fix: explorer urls renamed

--- src/verification/explorers.ts
+++ src/verification/explorers.ts
@@ -1,6 +1,13 @@
 import { NetworkType } from "../core/chains";
 
-export const explorerUrl: Record<NetworkType, string> = {
+export const explorerUrls: Record<NetworkType, string> = {
+  Mainnet: "https://etherscan.io",
+  Arbitrum: "https://arbiscan.io",
+  Optimism: "https://optimistic.etherscan.io",
+  Base: "https://basescan.org",
+};
+
+export const explorerApiUrls: Record<NetworkType, string> = {
   Mainnet: "https://api.etherscan.io/api",
   Arbitrum: "https://api.arbiscan.io/api",
   Optimism: "https://api-optimistic.etherscan.io/api",
