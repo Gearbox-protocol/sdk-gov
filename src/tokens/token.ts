79c4ad46e4a7afa8cd6fe7da771cd48b99aecda8
Arkhip Vouba <80775286+57Ark@users.noreply.github.com>
2025-01-22 12:19:42 +0100

    fix: update tickers

--- src/tokens/token.ts
+++ src/tokens/token.ts
@@ -1538,8 +1538,8 @@ export const tickerInfoTokensByNetwork: Record<
       {
         symbol: "beraSTONE/ETH",
         dataId: "beraSTONE_FUNDAMENTAL",
-        address: NOT_DEPLOYED,
-        priceFeed: NOT_DEPLOYED,
+        address: "0x692072eE081c9Ded28d2CfD27513E578d895FD0d",
+        priceFeed: "0xD35f2Fe7cc74e143F3f526c6e2c399fE58e31Cf1",
         reserve: false,
       },
     ],
