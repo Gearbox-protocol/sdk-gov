e57050f7a6e0ff20f9cdbff3247f7813c4e7d053
esser <blizzz93@gmail.com>
2025-02-14 17:33:38 +0800

    fix: pt bera

--- src/tokens/token.ts
+++ src/tokens/token.ts
@@ -1588,7 +1588,7 @@ export const tickerInfoTokensByNetwork: Record<
       {
         symbol: "beraSTONE/ETH",
         dataId: "beraETH_FUNDAMENTAL",
-        address: "0x692072eE081c9Ded28d2CfD27513E578d895FD0d",
+        address: tokenDataByNetwork.Mainnet.beraSTONE,
         priceFeed: "0xf12b7879713FA998B39b3C7C9c2631E40a0b9dfB",
         reserve: false,
       },
